60c4578e6fc6fe5be3224ee6c2de0d74eb61537f
Javith <javithumuhamed@gmail.com>
2021-01-28 12:52:09 +0530

    Header +assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from '.\\components\\Header.js';
 
 function App() {
   return (
     <div className="App">
-      
+
+    <Header/>
+      {/*Header*/}
+      {/*items<> snappng*/}
+      {/*items<> snappng*/}
+      {/*items<> snappng*/}
+    
     </div>
   );
 }
